60b53c65ef7fedb16081908c4e2719b02e03f0b4
Gustavo Alfaro <gstvag@gmail.com>
2020-09-23 23:10:38 -0600

    initial commit

---
+++ Readme.md
@@ -0,0 +1,17 @@
+Abtis App
+
+# Installation
+To start working run the next commands if you haven't installed nativescript yet
+
+```bash
+npm install -g nativescript
+npm install
+```
+
+# Usage
+
+To run the native playground use the next command
+
+```bash
+tns preview
+```
---
+++ app/App.svelte
@@ -0,0 +1,24 @@
+<page statusBarStyle="dark">
+    <actionBar title="El Puro App Papu" />
+    <gridLayout>
+        <label class="info" horizontalAlignment="center" verticalAlignment="middle" textWrap="true">
+            <formattedString>
+                <span class="fas" text="&#xf135;" />
+                <span text=" {message}" />
+            </formattedString>
+        </label>
+    </gridLayout>
+</page>
+
+<script lang="typescript">
+    let message: string = "El mero abits android"
+</script>
+
+<style>
+    .info .fas {
+        color: #3A53FF;
+    }
+    .info {
+        font-size: 20;
+    }
+</style>
